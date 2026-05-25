211304d2490b2bd82610380de28fc750bf46ff74
jerinjv32 <jerinjvarghese32@gmail.com>
2026-05-25 13:46:43 +0530

    Key is need to be collected for completing the game

--- scripts/game_manger.gd
+++ scripts/game_manger.gd
@@ -1,5 +1,7 @@
 extends Node
+
 @onready var global_timer: Timer = %GlobalTimer
+var key_is_collected: bool = false
 
 var coins: int = 0
 
@@ -9,3 +11,7 @@ func collect_coins() -> void:
 
 func _on_global_timer_timeout() -> void:
 	get_tree().change_scene_to_file('uid://bi70wo6tu0hua')
+
+
+func _on_key_key_is_collected() -> void:
+	key_is_collected = true
